552269d82c2882f75dca087f0460140341e02d80
mamund <mamund@yahoo.com>
2026-04-30 10:17:55 -0400

    Update links

--- build.sh
+++ build.sh
@@ -7,6 +7,13 @@ git commit -m"Update links"
 git push origin main
 
 # copy the index.html to the links folder and commit
+cp index.html ../../../github-webs/01-active/mamund.github.io/links/ .
+cd ../../../github-webs/01-active/mamund.github.io
+# git add --all
+# git commit -m"Update links"
+# git push origin main
+
+cd ../../../Private/Projects/2026-04-30/links-page
 
 # eof
 
