609e5e5b44e819b915cf9cf87b6fa57b3c499bfe
mamund <mamund@yahoo.com>
2026-04-30 10:20:26 -0400

    Update links

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 # build and commit links page updates
 
 # update the links page and commit
-node generate-links.page.js 
+node generate-links-page.js 
 git add --all
 git commit -m"Update links"
 git push origin main
